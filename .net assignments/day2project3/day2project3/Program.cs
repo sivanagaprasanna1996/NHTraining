e5ff49b0cfda9883d6ca8daed897a43218326931
IndraReddy007 <indrareddys1234@gmail.com>
2022-01-26 07:52:04 +0530

    day2projects

---
+++ .net assignments/day2project3/day2project3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int i;
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
