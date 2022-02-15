2e5bd3eea998bdc890781dba68875baf2c292211
IndraReddy007 <indrareddys1234@gmail.com>
2022-02-16 01:57:36 +0530

    day17

---
+++ day17assignments/day17pro1/PublicLibrary/MyPublicDerivedClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SivaLibrary;
+
+
+namespace PublicLibrary
+{
+    public class MyPublicDerivedClass : MyBaseClass
+    {
+        public void MyPublicDerivedClassMethod()
+        {
+            a = 5;
+            b = 10;
+            c = 15;
+            d = 20;
+            e = 25;
+       }
+    }
+   
+    public class MyPublicLibraryOtherClass
+    {
+        public void MyPublicLoibraryOtherClass()
+        {
+            MyBaseClass mb = new MyBaseClass();
+
+            mb.a = 5;
+            mb.b = 10;
+            mb.c = 15;
+            mb.d = 20;
+            mb.e = 25;
+
+        }
+    }
+}
---
+++ day17assignments/day17pro1/SivaLibrary/MyBaseClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SivaLibrary
+{
+    public class MyBaseClass
+    {
+        
+       
+           public int a;
+            private int b;
+            protected int c;
+            internal int d;
+            protected internal int e;
+         
+            public void MyBaseClassMethod()
+            {
+                a = 5;
+                b = 10;
+                c = 15;
+                d = 20;
+                e = 25;
+               
+
+            }
+        }
+
+        public class MyDerivedClass : MyBaseClass
+        {
+            public void MyderivedClassMethod()
+            {
+                
+                a = 5;
+                b = 10;
+                c = 15;
+                d = 20;
+                e = 25;
+               
+            }
+        }
+
+            public class MyOtherClass
+        {
+            public void MyOtherClassMethod()
+            {
+                
+                MyBaseClass mb = new MyBaseClass();
+                mb.a = 5;
+                mb.b = 10;
+                mb.c = 15;
+                mb.d = 20;
+                mb.e = 25;
+               
+
+            }
+
+
+        }
+
+    }
+}
