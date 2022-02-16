a9288f1af4b20dc2922033becdada93c4af8c7b5
IndraReddy007 <indrareddys1234@gmail.com>
2022-02-17 01:40:52 +0530

    day18 submission

---
+++ day18assignments/day18pro1/MathematicsLibraryTests/AlgebraTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathematicsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsLibrary.Tests
+{
+    [TestClass()]
+    public class AlgebraTests
+    {
+        [TestMethod()]
+        public void FactorialTest_Zero_input()
+        {
+            //Arrange
+            int n = 0;
+            int expected = 1;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void FactorialTest_One_To_Seven_Input()
+        {
+            //Arrange
+            int n = 6;
+            int expected = 720;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void FactorialTest_greater_than_Seven_Input()
+        {
+            //Arrange
+            int n = 11;
+            int expected = -999;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FactorialTest_Negitive_Input()
+        {
+            //Arrange
+            int n = -4;
+            int expected = -9999;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+  }
---
+++ day18assignments/day18pro1/day18pro1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathematicsLibrary;
+   
+
+namespace day18pro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Algebra.Factorial(n));
+            Console.ReadLine();
+
+        }
+    }
+}
