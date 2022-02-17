f14caf27e08d8ded5dd1d5429b496ae97a11b3f1
IndraReddy007 <indrareddys1234@gmail.com>
2022-02-18 01:36:45 +0530

    submission

---
+++ day19assignments/DAY19PRO2/DAY19PRO2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DAY19PRO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("file:///E:/assignments/day19assignments/Employee.xml.txt");
+            //  XmlNode node = doc.DocumentElement.SelectSingleNode("/employees/employee");
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "Name")
+                    {
+                        Console.WriteLine(childNode.InnerText);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ day19assignments/day19pro3/day19pro3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace day19pro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter ID Number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+
+            XmlDocument file = new XmlDocument();
+            file.Load("file:///E:/assignments/day19assignments/Employee.xml.txt");
+
+
+            foreach (XmlNode node in file.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode node2 in node.ChildNodes)
+                {
+                    bool id = node2.Name == "id";
+                    bool isIndex = (id == true ? Convert.ToInt32(node2.InnerText) : 0) == input;
+                    if (id && isIndex)
+                    {
+                        Console.WriteLine($"The Name of the ID is {node2.NextSibling.InnerText}.");
+                    }
+                }
+            }
+            Console.ReadLine();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
+
+
+
+
